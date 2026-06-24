f341c06cbb772fbbd4bc9c62504da87b2c905054
Koushik Tummepalli <koushikt.cbook@gmail.com>
2026-06-24 12:12:15 -0700

    Update app.py

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ logging.basicConfig(level=logging.INFO)
 
 # Initialize Slack Bolt App
 app = App(
-    token="xoxb-2210535565-11393247973702-pQynW3aq5sZjcRlnfq7GlKpx",
-    signing_secret="jNROyrtH4R3ekGMbUvotAg6R"
+    token="xoxb-2210535565-11393247973702-pQynW3aq5sZjcRlnfq7GlKpx"
+    signing_secret="99a358f606d3f51a5e18e37bd0b2d679"
 )
 handler = SlackRequestHandler(app)
 
